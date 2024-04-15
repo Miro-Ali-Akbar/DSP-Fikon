22ff7c358b7641587821d8b4534bdab086f81277
William Mårback <william.marback@hotmail.com>
2024-04-15 10:25:32 +0200

    Dynamic geofencing

--- trailquest/lib/main.dart
+++ trailquest/lib/main.dart
@@ -1,15 +1,18 @@
 import 'dart:async';
+import 'dart:io';
+// import 'dart:math' show cos, sqrt, asin;
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_maps_routes/google_maps_routes.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:geofence_service/geofence_service.dart' hide LocationPermission;
 
 WebSocketChannel? channel;
 
 void main() {
-  channel = WebSocketChannel.connect(Uri.parse("ws://localhost:3000"));
+  channel = WebSocketChannel.connect(Uri.parse("ws://192.168.46.200:3000"));
   runApp(const MyApp());
 }
 
@@ -27,7 +30,7 @@ void _sendMessage(String message) {
   }
 }
 
-Future<Position> _determinePosition() async {
+Future<dynamic> _requestLocationPermission() async {
   bool serviceEnabled;
   LocationPermission permission;
 
@@ -58,6 +61,10 @@ Future<Position> _determinePosition() async {
     return Future.error(
         'Location permissions are permanently denied, we cannot request permissions.');
   }
+}
+
+Future<Position> _determinePosition() async {
+  _requestLocationPermission().catchError(print);
 
   // When we reach here, permissions are granted and we can
   // continue accessing the position of the device.
@@ -90,19 +97,33 @@ class MapsRoutesExample extends StatefulWidget {
 class _MapsRoutesExampleState extends State<MapsRoutesExample> {
   late GoogleMapController mapController;
   final Completer<GoogleMapController> _controller = Completer();
+  final _activityStreamController = StreamController<Activity>();
+  final _geofenceStreamController = StreamController<Geofence>();
+  var position;
+  int activeIndex = 0;
+  bool isInArea = false;
 
-  Position? pos;
+  // Create a [GeofenceService] instance and set options.
+  final _geofenceService = GeofenceService.instance.setup(
+      interval: 2000,
+      accuracy: 10,
+      loiteringDelayMs: 60000,
+      statusChangeDelayMs: 10000,
+      useActivityRecognition: true,
+      allowMockLocations: true,
+      printDevLog: false,
+      geofenceRadiusSortType: GeofenceRadiusSortType.DESC);
 
   List<LatLng> points = [
-    const LatLng(59.84429621673012, 17.638228177426775),
-    const LatLng(59.84643220507929, 17.63963225848925),
-    const LatLng(59.85009456157953, 17.643307828593034),
-    const LatLng(59.85444306179348, 17.63943133739685),
+    const LatLng(59.85747242495416, 17.625860077218743),
+    const LatLng(59.858159646672945, 17.627228655854065),
+    const LatLng(59.858519279278624, 17.629492615895174),
+    const LatLng(59.85941849274688, 17.625949593271745),
   ];
 
   MapsRoutes route = MapsRoutes();
   DistanceCalculator distanceCalculator = DistanceCalculator();
-  String googleApiKey = 'AIzaSyDGApuCFwGexIAwxNd8qZETgZhC-xEdpYo';
+  String googleApiKey = 'API-KEY';
   String totalDistance = 'No route';
 
   Future<void> centerScreen(Position position) async {
@@ -111,12 +132,97 @@ class _MapsRoutesExampleState extends State<MapsRoutesExample> {
         CameraUpdate.newLatLng(LatLng(position.latitude, position.longitude)));
   }
 
+  // This function is to be called when the geofence status is changed.
+  Future<void> _onGeofenceStatusChanged(
+      Geofence geofence,
+      GeofenceRadius geofenceRadius,
+      GeofenceStatus geofenceStatus,
+      Location location) async {
+    print('geofence: ${geofence.toJson()}');
+    print('geofenceRadius: ${geofenceRadius.toJson()}');
+    print('geofenceStatus: ${geofenceStatus.toString()}');
+    _geofenceStreamController.sink.add(geofence);
+
+    if (geofenceStatus.toString() == "GeofenceStatus.ENTER" &&
+        geofence.toJson().toString().contains(RegExp('loc_$activeIndex'))) {
+      print("Entered area");
+      setState(() {
+        isInArea = true;
+      });
+    } else if (geofenceStatus.toString() == "GeofenceStatus.EXIT") {
+      print("Left area");
+
+      if (activeIndex == 0) {
+        print("Entered if");
+        _geofenceService.removeGeofenceById('loc_${points.length - 1}');
+      } else {
+        _geofenceService.removeGeofenceById('loc_${activeIndex - 1}');
+      }
+
+      setState(() {
+        isInArea = false;
+      });
+    }
+  }
+
+  // This function is to be called when the activity has changed.
+  void _onActivityChanged(Activity prevActivity, Activity currActivity) {
+    print('prevActivity: ${prevActivity.toJson()}');
+    print('currActivity: ${currActivity.toJson()}');
+    _activityStreamController.sink.add(currActivity);
+  }
+
+  // This function is to be called when the location has changed.
+  void _onLocationChanged(Location location) {
+    print('location: ${location.toJson()}');
+  }
+
+  // This function is to be called when a location services status change occurs
+  // since the service was started.
+  void _onLocationServicesStatusChanged(bool status) {
+    print('isLocationServicesEnabled: $status');
+  }
+
+  // This function is used to handle errors that occur in the service.
+  void _onError(error) {
+    final errorCode = getErrorCodesFromError(error);
+    if (errorCode == null) {
+      print('Undefined error: $error');
+      return;
+    }
+
+    print('ErrorCode: $errorCode');
+  }
+
   @override
   void initState() {
+    _requestLocationPermission().catchError(print);
     Geolocator.getPositionStream().listen((position) {
       centerScreen(position);
+      _sendMessage("$position");
     });
+
+    () async { while (true) {
+      _sendMessage(position);
+      sleep(Duration(seconds: 5));
+    }};
+    
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _geofenceService
+          .addGeofenceStatusChangeListener(_onGeofenceStatusChanged);
+      _geofenceService.addLocationChangeListener(_onLocationChanged);
+      _geofenceService.addLocationServicesStatusChangeListener(
+          _onLocationServicesStatusChanged);
+      _geofenceService.addActivityChangeListener(_onActivityChanged);
+      _geofenceService.addStreamErrorListener(_onError);
+      _geofenceService.start().catchError(_onError);
+    });
+    _geofenceService.addGeofence(Geofence(
+        id: "loc_$activeIndex",
+        latitude: points[activeIndex].latitude,
+        longitude: points[activeIndex].longitude,
+        radius: [GeofenceRadius(id: "radius_10m", length: 10)]));
   }
 
   @override
@@ -126,74 +232,143 @@ class _MapsRoutesExampleState extends State<MapsRoutesExample> {
         useMaterial3: true,
         colorSchemeSeed: Colors.green[700],
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('TrailQuest'),
-          elevation: 2,
-        ),
-        body: Stack(
-          children: [
-            Align(
-                alignment: Alignment.center,
-                child: StreamBuilder<Position>(
-                  stream: Geolocator.getPositionStream(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final position = snapshot.data;
-                      return GoogleMap(
-                        zoomControlsEnabled: false,
-                        myLocationEnabled: true,
-                        myLocationButtonEnabled: false,
-                        polylines: route.routes,
-                        initialCameraPosition: CameraPosition(
-                          target:
-                              LatLng(position!.latitude, position.longitude),
-                          zoom: 15.0,
-                        ),
-                        onMapCreated: (GoogleMapController controller) {
-                          _controller.complete(controller);
-                        },
-                      );
-                    }
-                    return const CircularProgressIndicator();
-                  },
-                )),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                    width: 200,
-                    height: 50,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15.0)),
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(totalDistance,
-                          style: const TextStyle(fontSize: 25.0)),
-                    )),
-              ),
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () async {
-            await route.drawRoute(points, 'Test routes',
-                const Color.fromRGBO(130, 78, 210, 1.0), googleApiKey,
-                travelMode: TravelModes.walking);
-            setState(() {
-              totalDistance = distanceCalculator.calculateRouteDistance(points,
-                  decimals: 1);
-            });
-            _determinePosition().then((value) {
-              mapController.animateCamera(CameraUpdate.newLatLngZoom(
-                  LatLng(value.latitude, value.longitude), 14));
-              _sendMessage("$value");
-            });
+      home: WillStartForegroundTask(
+          onWillStart: () async {
+            return _geofenceService.isRunningService;
           },
-        ),
-      ),
+          androidNotificationOptions: AndroidNotificationOptions(
+            channelId: 'geofence_service_notification_channel',
+            channelName: 'Geofence Service Notification',
+            channelDescription:
+                'This notification appears when the geofence service is running in the background.',
+            channelImportance: NotificationChannelImportance.LOW,
+            priority: NotificationPriority.LOW,
+            isSticky: false,
+          ),
+          iosNotificationOptions: const IOSNotificationOptions(),
+          foregroundTaskOptions: const ForegroundTaskOptions(),
+          notificationTitle: 'Geofence Service is running',
+          notificationText: 'Tap to return to the app',
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('TrailQuest'),
+              elevation: 2,
+            ),
+            body: Stack(
+              children: [
+                Align(
+                    alignment: Alignment.center,
+                    child: StreamBuilder<Position>(
+                      stream: Geolocator.getPositionStream(),
+                      builder: (context, snapshot) {
+                        // FIXME: Must reload app upon confirming permissions
+                        if (snapshot.hasData) {
+                          position = snapshot.data;
+                          return GoogleMap(
+                            zoomControlsEnabled: false,
+                            myLocationEnabled: true,
+                            myLocationButtonEnabled: false,
+                            polylines: route.routes,
+                            initialCameraPosition: CameraPosition(
+                              target: LatLng(
+                                  position!.latitude, position.longitude),
+                              zoom: 15.0,
+                            ),
+                            onMapCreated: (GoogleMapController controller) {
+                              _controller.complete(controller);
+                            },
+                            markers: {
+                              Marker(
+                                  markerId: MarkerId("hello"),
+                                  position: LatLng(points[activeIndex].latitude,
+                                      points[activeIndex].longitude))
+                            },
+                          );
+                        }
+                        return const CircularProgressIndicator();
+                      },
+                    )),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Container(
+                        width: 200,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15.0)),
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Text(totalDistance,
+                              style: const TextStyle(fontSize: 25.0)),
+                        )),
+                  ),
+                )
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              onPressed: () async {
+                if (isInArea) {
+                  if (activeIndex < points.length - 1) {
+                    await route.drawRoute(
+                        [points[activeIndex], points[activeIndex + 1]],
+                        'Test routes',
+                        const Color.fromRGBO(130, 78, 210, 1.0),
+                        googleApiKey,
+                        travelMode: TravelModes.walking);
+                    setState(() {
+                      totalDistance = distanceCalculator.calculateRouteDistance(
+                          [points[activeIndex], points[activeIndex + 1]],
+                          decimals: 1);
+                      activeIndex++;
+                      isInArea = false;
+                      _geofenceService.addGeofence(Geofence(
+                        id: "loc_$activeIndex",
+                        latitude: points[activeIndex].latitude,
+                        longitude: points[activeIndex].longitude,
+                        radius: [
+                          GeofenceRadius(id: 'radius_10m', length: 10),
+                        ],
+                      ));
+                    });
+                  } else {
+                    print(
+                        "End of points reached\nReset back to original point");
+                    await route.drawRoute(
+                        [points[activeIndex], points[0]],
+                        'Test routes',
+                        const Color.fromRGBO(130, 78, 210, 1.0),
+                        googleApiKey,
+                        travelMode: TravelModes.walking);
+                    setState(() {
+                      totalDistance = distanceCalculator.calculateRouteDistance(
+                          [points[activeIndex], points[0]],
+                          decimals: 1);
+                      activeIndex = 0;
+                      isInArea = false;
+                      _geofenceService.addGeofence(Geofence(
+                        id: "loc_$activeIndex",
+                        latitude: points[activeIndex].latitude,
+                        longitude: points[activeIndex].longitude,
+                        radius: [
+                          GeofenceRadius(id: 'radius_10m', length: 10),
+                        ],
+                      ));
+                    });
+                  }
+                } else {
+                  print("Not in area");
+                }
+                // _determinePosition().then((value) {
+                //   mapController.animateCamera(CameraUpdate.newLatLngZoom(
+                //       LatLng(value.latitude, value.longitude), 14));
+                //   // _sendMessage("$value");
+                // });
+                // _sendMessage("$position");
+              },
+            ),
+          )),
     );
   }
 }
--- trailquest/test/widget_test.dart
+++ trailquest/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:trailquest/main.dart';
+import 'package:trailquest/main.dart.new';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
