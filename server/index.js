d0d0e2fab9547f241883b80ca19365eb31508c19
AlexanderLiberman <4lex030@gmail.com>
2024-04-18 10:01:15 +0200

    Server skeleton

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { Server } = require('ws');
+const { Server } = require('ws');
 const { initializeApp, applicationDefault, cert} = require('firebase-admin/app');
 const  { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 const serviceAccount = require("./serviceAccountKey.json");
@@ -23,21 +23,34 @@ const wss = new Server({server});
 
 
 // Server helper functions
+
+/**
+ * @brief Updates alive-status of a socket.
+ */
 function heartbeat() {
     this.isAlive = true;
 }
 
+/**
+ * @brief ID generation for new websockets
+ * @returns generated user ID
+ */
 function generateID() {
     return Math.floor((1 + Math.random()) * 0x10000);
 }
 
 
 // Database helper functions
+
+/**
+ * @brief Enters data into a given document in database
+ * @param userData data given to the function from parsed JSON-string
+ */
 async function initUser(userData) {
     await usersRef.doc("hitsu").set(userData);
 }
 
-
+wss.connectedUsers = [];
 // Event handler
 
 wss.on('connection', ws => {
@@ -56,17 +69,19 @@ wss.on('connection', ws => {
     // ws.isAlive = true;
 
     // ws.on('pong', heartbeat);
-    
+
     ws.on('message', function inc(data) {
         let message =  data;
 
         message = JSON.parse(message);
 
         console.log(message);
+        console.log(message.data);
         switch(message.msgID) {
             case "initRes":
-                initUser(message);
+                initUser(message.data);
                 console.log('added data');
+                wss.connectedUsers[0] = [message.data.signature, ws.id];
                 break;
         }
     })
