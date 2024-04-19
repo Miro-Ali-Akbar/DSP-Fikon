367ffc14e6521a0a486af4b7ea68e0196fe5e574
William Mårback <william.marback@hotmail.com>
2024-04-19 14:19:33 +0200

    Map exists, but no stacking? Needs to inspect why

--- client/lib/pages/start_page.dart
+++ client/lib/pages/start_page.dart
@@ -1,34 +1,69 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class StartPage extends StatelessWidget{
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class StartPage extends StatefulWidget {
   const StartPage({super.key});
- 
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  late Completer<GoogleMapController> _controller = Completer();
+
+  String googleApiKey = 'API-KEY';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
       home: Scaffold(
-
         appBar: AppBar(
           actions: const [],
           title: const Align(
             alignment: Alignment.centerRight,
-            child: Text('TrailQuest', 
-              style: TextStyle(
-              color: Colors.black),
-              ),
+            child: Text(
+              'TrailQuest',
+              style: TextStyle(color: Colors.black),
+            ),
           ),
         ),
-
-        body: const Center(
-          child: Text('Start!', 
-            style: TextStyle(
-              color: Colors.green, 
-              fontSize: 30.0,
-            )
-          ), 
+        body: Stack(
+          children: [
+            ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Card(
+                  child: Placeholder(),
+                ),
+                Card(
+                  child: Placeholder(),
+                ),
+                Card(
+                  child: Placeholder(),
+                )
+              ],
+            ),
+            GoogleMap(
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              zoomControlsEnabled: false,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              initialCameraPosition: CameraPosition(
+                  target: LatLng(59.83972677529924, 17.6465716818546),
+                  zoom: 20),
+              markers: {
+                Marker(
+                  markerId: MarkerId("Ångan"),
+                  position: LatLng(59.83972677529924, 17.6465716818546),
+                ),
+              },
+            ),
+          ],
         ),
       ),
     );
