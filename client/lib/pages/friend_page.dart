36b1f8fcdb0ff25cee0369eee6e8a7b3cb168369
santabeard <emmamarti@live.se>
2024-04-25 17:07:13 +0200

    start

---
+++ client/lib/pages/friend_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+
+
+class Friendpage extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+        
+
+      )
+    );
+
+  }
+
+}
