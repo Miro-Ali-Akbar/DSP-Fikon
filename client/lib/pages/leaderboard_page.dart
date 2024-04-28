0ac5bef252489356d7979d61e9a55d3d87c32e87
santabeard <emmamarti@live.se>
2024-04-28 21:02:42 +0200

    Update leaderboard_page.dart

--- client/lib/pages/leaderboard_page.dart
+++ client/lib/pages/leaderboard_page.dart
@@ -44,11 +44,7 @@ class Leaderboard extends StatelessWidget {
                     );
                   }
                 ),
-              )
-              
-              
-              
-            
+              ) 
           ],
         )   
       )
@@ -56,23 +52,3 @@ class Leaderboard extends StatelessWidget {
   }
 }
 
-
-class BackButton extends StatelessWidget {
-
-  @override 
-  Widget build(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () {
-        Navigator.pop(context); 
-      }, 
-
-      icon: SvgPicture.asset('assets/images/arrow-sm-left-svgrepo-com (1).svg',
-        width: 40,
-        height: 40,
-        colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn)
-      ),
-
-      label: Text(''), 
-    ); 
-  }
-}
