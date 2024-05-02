330cd16ffe48e433427dd8546887c8cb6ce7de1e
santabeard <emmamarti@live.se>
2024-05-02 09:59:10 +0200

    rough first try

--- client/lib/pages/friend_page.dart
+++ client/lib/pages/friend_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
+import 'package:flutter_svg/svg.dart';
 
 
 class Friendpage extends StatelessWidget {
@@ -10,8 +10,37 @@ class Friendpage extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
-        
-
+        appBar: AppBar(
+          leading: BackButton(),
+          title: Text("Friends",
+            style: TextStyle(
+              color: Colors.green, 
+              fontSize: 30.0
+            )
+          
+          ),
+           actions: <Widget>[
+            TextButton.icon(
+                onPressed: () {
+  
+                  
+                },
+                label: Text(
+                  'Add Friend +',
+                  style: TextStyle(color: Colors.white),
+                ),
+                icon: SvgPicture.asset('assets/images/img_group.svg'),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green.shade600,
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 10, horizontal: 30),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                )
+              ),
+           ]
+        )
       )
     );
 
