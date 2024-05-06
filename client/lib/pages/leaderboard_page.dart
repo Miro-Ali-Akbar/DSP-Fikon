685925be98b9345741c7dce6b48c07e07b2a37af
Nora Reneland <nora.reneland.1770@student.uu.se>
2024-05-06 10:02:34 +0200

    formated the trail cards with pictures

--- client/lib/pages/leaderboard_page.dart
+++ client/lib/pages/leaderboard_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:trailquest/challenges_list.dart';
+import 'package:trailquest/widgets/back_button.dart';
 import 'package:trailquest/widgets/challenge_cards.dart';
 
 
@@ -26,7 +27,7 @@ class Leaderboard extends StatelessWidget {
             
             Row(
               children: [
-                BackButton().build(context),
+                GoBackButton().build(context),
                 Text('Leaderboard',
                     style: TextStyle(
                       color: Colors.green, 
